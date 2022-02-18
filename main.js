b7cbb4fb141d8b0f99475eacccb6bb67c5db2c80
juancarloscr17 <juankcr17@gmail.com>
2022-02-17 21:47:16 -0500

    Funcion por Expresion

---
+++ main.js
@@ -0,0 +1,10 @@
+// Funcion por Expresion
+function saludarRobot(num) {
+let nombre = prompt(`Hola soy el robot de JS como te llamas usuario numero ${num}`)
+alert(`Hola ${nombre} mucho gusto conocerte`);
+}
+saludarRobot(1);
+saludarRobot(2);
+saludarRobot(3);
+saludarRobot(4);
+saludarRobot(5);
