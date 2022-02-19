d74ee6286958674ff93970417e31cb7f6ab60179
juancarloscr17 <juankcr17@gmail.com>
2022-02-19 07:40:59 -0500

    Funcion por Declaracion

--- main.js
+++ main.js
@@ -1,10 +1,25 @@
-// Funcion por Expresion
-function saludarRobot(num) {
-let nombre = prompt(`Hola soy el robot de JS como te llamas usuario numero ${num}`)
-alert(`Hola ${nombre} mucho gusto conocerte`);
-}
-saludarRobot(1);
-saludarRobot(2);
-saludarRobot(3);
-saludarRobot(4);
-saludarRobot(5);
+//Funcion por Declaracion
+const saludar = function saludarRobot(num) {
+    let nombre = prompt(`Hola soy el robot del JS como te llamas usuario ${num}`);
+    alert(`Hola usuario ${nombre} me alegra conocerte`);
+};
+saludar(1);
+saludar(2);
+saludar(3);
+saludar(4);
+saludar(5);
+
+
+
+
+
+// // Funcion por Expresion
+// function saludarRobot(num) {
+// let nombre = prompt(`Hola soy el robot de JS como te llamas usuario numero ${num}`)
+// alert(`Hola ${nombre} mucho gusto conocerte`);
+// }
+// saludarRobot(1);
+// saludarRobot(2);
+// saludarRobot(3);
+// saludarRobot(4);
+// saludarRobot(5);
